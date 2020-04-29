da1ba6a00ab5d341fcc3e834a32ecbe31f5600a9
GaryHjy <782316959@qq.com>
2020-04-29 23:16:21 +0800

    feat: 添加getStyleLoaders方法

--- build/utils.js
+++ build/utils.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 require('dotenv').config();
 
 ['NODE_ENV', 'API_SERVER', 'PUBLIC_PATH'].forEach(key => {
@@ -9,6 +10,45 @@ require('dotenv').config();
 const appDirectory = fs.realpathSync(process.cwd());
 const resolvePath = relativePath => path.resolve(appDirectory, relativePath);
 const publicPath = process.env.PUBLIC_PATH || './'
+const isDev = process.env.NODE_ENV === 'development';
+
+/**
+ * @description 获取styleLoader配置
+ * @param {object} cssOptions css-loader配置
+ * @param {string} preProcessor 预处理器loader
+ */
+const getStyleLoaders = function (cssOptions, preProcessor) {
+  const loaders = [
+    isDev && require.resolve('style-loader'),
+    !isDev && {
+      loader: MiniCssExtractPlugin.loader,
+      options: publicPath.startsWith('.')
+        ? { publicPath: '../../' }
+        : {},
+    },
+    {
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    },
+    {
+      loader: require.resolve('postcss-loader'),
+      options: {
+        sourceMap: !isDev
+      },
+    },
+  ].filter(Boolean);
+  if (preProcessor) {
+    loaders.push(
+      {
+        loader: require.resolve(preProcessor),
+        options: {
+          sourceMap: true,
+        },
+      }
+    );
+  }
+  return loaders;
+}
 
 module.exports = {
   appEnv: process.env,
@@ -18,5 +58,6 @@ module.exports = {
   appHtml: resolvePath('public/index.html'),
   proxy: resolvePath('config/proxy.config.js'),
   mockServer: resolvePath('config/mock.js'),
-  publicPath
+  publicPath,
+  getStyleLoaders
 }

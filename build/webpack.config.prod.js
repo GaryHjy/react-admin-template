648af818798d552b1c53c8455c094319cdc0cc27
GaryHjy <782316959@qq.com>
2020-04-29 17:30:32 +0800

    feat: 添加webpack.config.prod文件

---
+++ build/webpack.config.prod.js
@@ -0,0 +1,26 @@
+const utils = require('./utils');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: utils.appHtml,
+      inject: true, // js插入的位置
+      minify: {
+        collapseWhitespace: true, // 折叠构成文档树中的文本节点的空白
+        removeComments: true, // 带HTML注释
+        removeStyleLinkTypeAttributes: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true, // 压缩css
+        minifyCSS: true,// 压缩js
+        minifyURLs: true,
+      }
+    })
+  ]
+}

548be62a6a685b68da841096045ae53e175c6b51
GaryHjy <782316959@qq.com>
2020-04-30 22:38:31 +0800

    feat: 修改webpack.base.config.js文件引入及css module处理

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
-const utils = require('./utils');
-const getStyleLoaders = require('./utils').getStyleLoaders;
+const paths = require('./paths');
+const { getStyleLoaders, getCSSModuleLocalIdent} = require('./utils');
 const devConfig = require('./webpack.config.dev');
 const prodConfig = require('./webpack.config.prod');
 
@@ -8,7 +8,7 @@ const config = {
   'development': devConfig,
   'production': prodConfig
 }
-const isDev = utils.appEnv.NODE_ENV === 'development';
+const isDev = paths.appEnv.NODE_ENV === 'development';
 
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
@@ -16,12 +16,12 @@ const lessRegex = /\.less$/;
 const lessModuleRegex = /\.module\.less$/;
 
 const baseConfig = {
-  entry: utils.appIndex,
+  entry: paths.appIndex,
   output: {
     pathinfo: isDev, // bundle中引入所包含模块信息的相关注释
     filename: isDev ? 'static/js/bundle.js' : 'static/js/[name].[chunkhash:8].js',
-    path: utils.appBuild,
-    publicPath: !isDev ? utils.publicPath : undefined,
+    path: paths.appBuild,
+    publicPath: !isDev ? paths.publicPath : undefined,
     chunkFilename: isDev ? 'static/js/[name].chunk.js' : 'static/js/[name].[contenthash:8].chunk.js'
   },
   module: {
@@ -40,19 +40,40 @@ const baseConfig = {
         sideEffects: true,
       },
       {
+        test: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+          modules: {
+            getLocalIdent: getCSSModuleLocalIdent
+          }
+        })
+      },
+      {
         test: lessRegex,
         exclude: lessModuleRegex,
         use: getStyleLoaders(
           {
             importLoaders: 2,
-            sourceMap: !isDev
+            sourceMap: !isDev,
           },
           'less-loader'
         ),
         sideEffects: true
       },
+      {
+        test: lessModuleRegex,
+        use: getStyleLoaders(
+          {
+            importLoaders: 2,
+            modules: {
+              getLocalIdent: getCSSModuleLocalIdent
+            }
+          },
+          'less-loader'
+        )
+      },
     ]
   }
 }
 
-module.exports = merge(baseConfig, config[utils.appEnv.NODE_ENV])
+module.exports = merge(baseConfig, config[paths.appEnv.NODE_ENV])

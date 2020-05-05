25c20e90017c463df68e129d82d2551854c9b6b9
GaryHjy <782316959@qq.com>
2020-05-05 16:45:36 +0800

    feat: 修改配置

--- postcss.config.js
+++ postcss.config.js
@@ -7,14 +7,14 @@ module.exports = ({ env }) => ({
     'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
       autoprefixer: {
-        flexbox: 'no-2009' // 兼容性
+        flexbox: 'no-2009', // 兼容性
       },
-      stage: 3
+      stage: 3,
     },
     // 擦除无用css
     '@fullhuman/postcss-purgecss': {
-      content: [ paths.appHtml, ...glob.sync(`${ paths.appSrc }/**/*.{js,jsx}`, { nodir: true }) ]
+      content: [paths.appHtml, ...glob.sync(`${paths.appSrc}/**/*.{js,jsx}`, { nodir: true })],
     },
-    'cssnano': env === 'production' ? {} : false // css压缩
-  }
-})
+    cssnano: env === 'production' ? {} : false, // css压缩
+  },
+});

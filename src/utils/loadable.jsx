52ee80ce41d13dc1b99cb7dcfa285aa912d698e8
GaryHjy <782316959@qq.com>
2020-05-06 21:20:47 +0800

    feat: 修改loadable配置

--- src/utils/loadable.jsx
+++ src/utils/loadable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import loadable from 'react-loadable';
 import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
 
 class LoadingComponent extends Component {
   constructor(props) {

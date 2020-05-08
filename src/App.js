ff3e3a0715d89dd64a066ca8a9d7776b95a7d8e8
GaryHjy <782316959@qq.com>
2020-05-08 19:32:45 +0800

    feat: 添加路由渲染组件

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import routes from '@/routes';
-import BasicLayout from '@/layouts/BasicLayout/index';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import RenderRoute from '@/components/RenderRoute/index';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          {routes.map((item) => {
-            const Layout = item.layout || BasicLayout;
-            return (
-              <Route
-                key={item.path}
-                path={item.path}
-                exact={item.exact}
-                render={() => (
-                  <Layout>
-                    <item.component />
-                  </Layout>
-                )}
-              />
-            );
-          })}
+          <Route path="/index" exact render={() => <Redirect to="/" />} />
+          <Route component={RenderRoute} />
         </Switch>
       </Router>
     );
---
+++ src/components/RenderRoute/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import routes from '@/routes';
+import BasicLayout from '@/layouts/BasicLayout/index';
+
+class RenderRoute extends Component {
+  render() {
+    return (
+      <>
+        {routes.map((item) => {
+          const Layout = item.layout || BasicLayout;
+          return (
+            <Route
+              key={item.path}
+              path={item.path}
+              exact={item.exact}
+              render={() => (
+                <Layout>
+                  <item.component {...this.props} />
+                </Layout>
+              )}
+            />
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default RenderRoute;

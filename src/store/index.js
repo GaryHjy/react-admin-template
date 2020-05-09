565c2c84f6bcc44c1fe349ec1a0881232db9824a
GaryHjy <782316959@qq.com>
2020-05-09 17:59:15 +0800

    feat: 添加redux-promise redux-thunk配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 import logger from 'redux-logger';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(logger));
+const store = createStore(reducers, applyMiddleware(promise, thunk, logger));
 
 export default store;

8bad2dfe456a124f22c7e6ab63f2417bd93c26ac
GaryHjy <782316959@qq.com>
2020-05-11 23:13:21 +0800

    feat: 添加user配置

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import permission from './permission';
 import setting from './setting';
+import user from './user';
 import history from '../history';
 
 const reducers = {
   permission,
+  user,
   setting,
   router: connectRouter(history),
 };

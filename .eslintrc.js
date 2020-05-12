d4e074e11dd5e133bab5c917657ae5006775c46f
GaryHjy <782316959@qq.com>
2020-05-13 00:02:26 +0800

    feat: 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     "prefer-spread": "off",
     "no-shadow": "off",
     "no-param-reassign": "off",
+    "no-case-declarations": "off",
     "semi": 0, //不强制使用分号
     "import/no-unresolved": [0, { commonjs: true, amd: true }], // 关闭 确保导入的模块可以按照标准节点要求解析为本地文件系统上的模块
     "no-irregular-whitespace": 0, //不规则的空白不允许

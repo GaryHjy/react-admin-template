aa005a0d87c4ba0689a160df7f12279c68cfa5eb
GaryHjy <782316959@qq.com>
2020-05-13 00:06:29 +0800

    feat: 修改user reducer

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,10 +1,15 @@
+import * as TYPES from '../actionTypes';
+
 const initialState = {
-  token: '',
+  accessToken: '',
   user: {},
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case TYPES.UPDATE_USER_INFO:
+      const { data } = action.payload;
+      return { ...state, accessToken: data.accessToken, user: { ...state.user, ...data } };
     default:
       return state;
   }

4871bfd26a2c412f1807d549d4642e080d6890fb
GaryHjy <782316959@qq.com>
2020-05-13 23:15:47 +0800

    feat: 修改HeadDropdown组件及处理退出操作

--- src/layouts/components/HeadDropdown.js
+++ src/layouts/components/HeadDropdown.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import { Menu, Dropdown, Avatar } from 'antd';
 import { UserOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons';
 import '@/styles/layout-styles/head-dropdown.less';
+import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
+import { USER_LOGOUT } from '../../store/actionTypes';
 
 class HeadDropdown extends Component {
   onMenuClick = event => {
     const { key } = event;
+    const { dispatch } = this.props;
 
     console.log(key);
 
-    // if (key === 'logout') {
-    //   const { dispatch } = this.props;
-
-    //   if (dispatch) {
-    //     dispatch({
-    //       type: 'login/logout',
-    //     });
-    //   }
-    // }
+    if (key === 'logout') {
+      sessionStorage.removeItem('accessToken');
+      // 清空store中用户数据
+      dispatch({
+        type: USER_LOGOUT,
+      });
+      // 跳转登录
+      dispatch(push('/login'));
+    }
   };
 
   render() {
+    const { user } = this.props;
     const menuHeaderDropdown = (
       <Menu className="menu" selectedKeys={[]} onClick={this.onMenuClick}>
         <Menu.Item key="center">
@@ -43,11 +48,13 @@ class HeadDropdown extends Component {
       <Dropdown className="head-dropdown" overlay={menuHeaderDropdown}>
         <span className="head-dropdown__content">
           <Avatar className="head-dropdown__content--avatar" size="small" icon={<UserOutlined />} alt="avatar" />
-          <span className="head-dropdown__content--name">username</span>
+          <span className="head-dropdown__content--name">{user.username}</span>
         </span>
       </Dropdown>
     );
   }
 }
 
-export default HeadDropdown;
+const mapStateToProps = state => state.user;
+
+export default connect(mapStateToProps)(HeadDropdown);

52949d00f4a808036b4ca73bc8617cc1fbb2c1b1
GaryHjy <782316959@qq.com>
2020-05-14 22:14:54 +0800

    feat: 修改loadable

--- src/utils/loadable.jsx
+++ src/utils/loadable.jsx
@@ -1,37 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import loadable from 'react-loadable';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css'; // 进度条css
+import LoadingComponent from '@/components/LoadingComponent';
+import WrappedLayout from '@/components/WrappedLayout/index';
 import BasicLayout from '@/layouts/BasicLayout/index';
-
-class LoadingComponent extends Component {
-  constructor(props) {
-    super(props);
-    NProgress.start();
-  }
-
-  componentDidMount() {
-    NProgress.done();
-  }
-
-  render() {
-    return <div />;
-  }
-}
 
 export default (loader, loading = LoadingComponent) =>
   loadable({
     loader,
     loading,
-    // eslint-disable-next-line react/no-multi-comp
     render(loaded, props) {
-      console.log(loaded, props);
       const Component = loaded.default;
       const Layout = Component.Layout || BasicLayout;
-      return (
-        <Layout {...props}>
-          <Component {...props} />
-        </Layout>
-      );
+      return <WrappedLayout Layout={Layout} Component={Component} {...props} />;
     },
   });

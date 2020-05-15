81b40135a60d25bdbb8b85b721706c37e37c61f8
GaryHjy <782316959@qq.com>
2020-05-15 17:49:38 +0800

    feat: 修改http.js

--- src/services/http.js
+++ src/services/http.js
@@ -16,9 +16,10 @@ const instance = axios.create({
 // http request 拦截器
 instance.interceptors.request.use(
   config => {
-    // if (store.state.token) {
-    //   config.headers.Authorization = `Bearer ${store.state.token}`;
-    // }
+    const token = sessionStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   err => Promise.reject(err),
@@ -29,15 +30,6 @@ instance.interceptors.response.use(
   res => {
     const { data } = res;
 
-    if (data.code === '207') {
-      // Message.error({
-      //   message: data.msg || '登录失效，请重新登录',
-      // });
-      // router.push('/login');
-      sessionStorage.removeItem('token');
-      return Promise.reject(data);
-    }
-
     if (data.code >= 400) {
       message.error(data.message);
       return Promise.reject(data);
@@ -45,16 +37,25 @@ instance.interceptors.response.use(
     return res;
   },
   error => {
-    const { status } = error.response;
+    const { status, data } = error.response;
     if (status && status === 403) {
       // Message.error({
       //   message: '当前操作无权限',
       // });
       return Promise.reject(error);
     }
-    // if (status && status >= 400) {
-    //   message.error(data.message);
-    // }
+    if (status === 401) {
+      message.error(data.message || '登录失效，请重新登录');
+      sessionStorage.removeItem('accessToken');
+      // 重新载入
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+      return Promise.reject(error);
+    }
+    if (status && status >= 400) {
+      message.error(data.message);
+    }
     return Promise.reject(error);
   },
 );

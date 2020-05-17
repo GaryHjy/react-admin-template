299c64fd7b7cfb6d5410c5e700bf72484b302293
GaryHjy <782316959@qq.com>
2020-05-17 18:48:54 +0800

    feat: 修改routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,17 +5,17 @@ export default [
     path: '/',
     name: 'home',
     exact: true,
-    component: loadable(() => import(/* webpackChunkName: 'index' */ '@/views/Index')),
+    component: loadable(() => import(/* webpackChunkName: 'index' */ '@/views/Index/index')),
   },
   {
     path: '/login',
     name: 'login',
     exact: true,
-    component: loadable(() => import(/* webpackChunkName: 'login' */ '@/views/Login')),
+    component: loadable(() => import(/* webpackChunkName: 'login' */ '@/views/Login/index')),
   },
   {
     path: '/404',
     name: '404',
-    component: loadable(() => import(/* webpackChunkName: '404' */ '@/views/Others/404/index.jsx')),
+    component: loadable(() => import(/* webpackChunkName: '404' */ '@/views/Others/404/index')),
   },
 ];

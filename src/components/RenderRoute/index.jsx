ebcd717acac618528d81f8f4881a0b6ebb0f9fca
GaryHjy <782316959@qq.com>
2020-05-17 18:42:53 +0800

    feat: 修改RenderRouter组件

--- src/components/RenderRoute/index.jsx
+++ src/components/RenderRoute/index.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import routes from '@/routes';
 
 class RenderRoute extends Component {
   render() {
     return (
-      <>
+      <Switch>
         {routes.map(item => {
           return (
             <Route
-              key={item.path}
-              path={item.path}
+              key={item.name}
+              path={item.path && item.path}
               exact={item.exact}
               render={() => <item.component {...this.props} />}
             />
           );
         })}
-      </>
+        <Redirect to="/404" />
+      </Switch>
     );
   }
 }

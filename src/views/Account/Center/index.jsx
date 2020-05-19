06dabab1e1ca9051ae6d81519b96c424525d6bae
GaryHjy <782316959@qq.com>
2020-05-19 17:54:44 +0800

    feat: 修改个人中心页面

--- src/views/Account/Center/index.jsx
+++ src/views/Account/Center/index.jsx
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import DataMapper from '@/components/DataMapper/index';
+import '@/styles/views/account-center.less';
 
-class PersonalCenter extends Component {
-  render() {
-    return <div>个人中心</div>;
-  }
+function AccountCenter(props) {
+  const columns = [
+    {
+      label: '用户名',
+      key: 'username',
+    },
+    {
+      label: '手机',
+      key: 'mobile',
+    },
+    {
+      label: '邮箱',
+      key: 'email',
+    },
+    {
+      label: '角色',
+      key: 'role',
+    },
+  ];
+  const data = props.store.user.user || {};
+  return (
+    <div className="account-center">
+      <DataMapper title="个人中心" columns={columns} data={data} />
+    </div>
+  );
 }
 
-export default PersonalCenter;
+export default AccountCenter;

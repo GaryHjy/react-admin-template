f0b38d273685a222d89236074fc7bc6d3475616e
GaryHjy <782316959@qq.com>
2020-05-22 11:24:49 +0800

    feat: 修改配置

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -79,12 +79,12 @@ const baseConfig = {
             include: paths.appSrc,
             exclude: paths.appNodeModules,
             use: [
-              {
-                loader: 'thread-loader',
-                options: {
-                  workers: 3, // 开启几个 worker 进程来处理打包，默认是 os.cpus().length - 1
-                }
-              },
+              // {
+              //   loader: 'thread-loader',
+              //   options: {
+              //     workers: 3, // 开启几个 worker 进程来处理打包，默认是 os.cpus().length - 1
+              //   }
+              // },
               {
                 loader: require.resolve('babel-loader'),
                 options: {

f7104c463b08346a651ac1577a21b0686fb38b5d
GaryHjy <782316959@qq.com>
2020-05-23 19:40:40 +0800

    feat: 添加account-setting页面

--- src/views/Account/Setting/index.jsx
+++ src/views/Account/Setting/index.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import { Menu } from 'antd';
+import styles from './style.module.less';
 
 class AccountSetting extends Component {
+  state = {
+    active: 'basic',
+    menuList: [
+      {
+        key: 'basic',
+        value: '基本信息',
+      },
+      {
+        key: 'security',
+        value: '安全设置',
+      },
+    ],
+  };
+
+  handleChange = ({ key }) => {
+    this.setState({
+      active: key,
+    });
+  };
+
   render() {
-    return <div>个人设置</div>;
+    const { menuList, active } = this.state;
+    return (
+      <div className={styles.setting}>
+        <div className={styles.setting__menu}>
+          <Menu mode="inline" defaultSelectedKeys={active} onSelect={this.handleChange}>
+            {menuList.map(menu => (
+              <Menu.Item key={menu.key}>{menu.value}</Menu.Item>
+            ))}
+          </Menu>
+        </div>
+        <div className={styles.setting__body}>{active}</div>
+      </div>
+    );
   }
 }
 

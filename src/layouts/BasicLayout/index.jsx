f5414f33f40425cfb687c6326553144a146ea9a5
GaryHjy <782316959@qq.com>
2020-05-24 18:00:08 +0800

    style: 修改BasicLayout引用css module

--- src/layouts/BasicLayout/index.jsx
+++ src/layouts/BasicLayout/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import '@/styles/layout-styles/basic-layout.less';
 import { Layout, Menu } from 'antd';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import LayoutHead from '../components/LayoutHead';
+import styles from './style.module.less';
 
 const { Content, Footer, Sider } = Layout;
 
@@ -13,7 +13,7 @@ class DefaultLayout extends Component {
     const { collapsed } = setting;
     return (
       <>
-        <Layout className="basic-layout">
+        <Layout className={styles['styles.basic-layout']}>
           <Sider trigger={null} collapsible collapsed={collapsed}>
             <div className="logo" />
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
@@ -33,9 +33,9 @@ class DefaultLayout extends Component {
           </Sider>
           <Layout>
             <LayoutHead />
-            <Content className="basic-layout__content">
-              <div className="basic-layout__content--children">{children}</div>
-              <Footer className="basic-layout__footer">Ant Design ©2018 Created by Ant UED</Footer>
+            <Content className={styles['basic-layout__content']}>
+              <div className={styles['basic-layout__content--children']}>{children}</div>
+              <Footer className={styles['basic-layout__footer']}>Ant Design ©2018 Created by Ant UED</Footer>
             </Content>
           </Layout>
         </Layout>

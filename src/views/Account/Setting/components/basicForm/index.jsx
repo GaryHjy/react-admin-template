533cf79c27bc640cfcd12f891d25bdf413381f18
GaryHjy <782316959@qq.com>
2020-05-27 20:13:51 +0800

    feat: basicForm添加css

--- src/views/Account/Setting/components/basicForm/index.jsx
+++ src/views/Account/Setting/components/basicForm/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
+import styles from './style.module.less';
 
 const BasicForm = () => {
   const [form] = Form.useForm();
@@ -9,40 +10,42 @@ const BasicForm = () => {
   };
 
   return (
-    <Form
-      form={form}
-      layout="vertical"
-      name="register"
-      onFinish={onFinish}
-      initialValues={{
-        residence: ['zhejiang', 'hangzhou', 'xihu'],
-        prefix: '86',
-      }}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="邮箱"
-        rules={[
-          {
-            type: 'email',
-            message: '请输入正确的邮箱格式',
-          },
-          {
-            required: true,
-            message: '请输入邮箱地址',
-          },
-        ]}
+    <div className={styles['basic-form']}>
+      <Form
+        form={form}
+        layout="vertical"
+        name="register"
+        onFinish={onFinish}
+        initialValues={{
+          residence: ['zhejiang', 'hangzhou', 'xihu'],
+          prefix: '86',
+        }}
+        scrollToFirstError
       >
-        <Input placeholder="请输入" />
-      </Form.Item>
+        <Form.Item
+          name="email"
+          label="邮箱"
+          rules={[
+            {
+              type: 'email',
+              message: '请输入正确的邮箱格式',
+            },
+            {
+              required: true,
+              message: '请输入邮箱地址',
+            },
+          ]}
+        >
+          <Input placeholder="请输入" />
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          保存设置
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            保存设置
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
